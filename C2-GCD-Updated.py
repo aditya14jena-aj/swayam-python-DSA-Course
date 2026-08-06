51b1a262a7fc9a786a5de13fad82159cc520fbe6
aditya14jena-aj <aditya14jena@gmail.com>
2026-08-06 20:00:56 +0530

    Fixed the logical error

--- C2-GCD-Updated.py
+++ C2-GCD-Updated.py
@@ -1,10 +1,10 @@
 def gcd(m,n):
 
     cf = [1,]
-    for i in range(1,max(m,n)):
+    for i in range(1,max(m,n)+1):
         if m % i == 0 and n % i == 0:
             cf.append(i)
-            
+
     return cf[-1]
     
 print(gcd(4,8))
